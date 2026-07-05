karagozemin/Fortexa

Add JSON request body size limits for sensitive API routes (fixes #56) (#57)
Co-authored-by: Muyideen-js <muyideen@fluxora.dev>
Co-authored-by: Cursor <cursoragent@cursor.com>

src/app/api/stellar/submit-signed/body-limit.test.ts
@@ -1,0 +1,55 @@
+import { describe, expect, it } from "vitest";
+import { NextRequest } from "next/server";
+
+import { POST } from "@/app/api/stellar/submit-signed/route";
+import { AUTH_COOKIE_KEY, createSessionToken } from "@/lib/auth/session";
+import { DEFAULT_JSON_BODY_MAX_BYTES } from "@/lib/http/read-json-body";
+
+function operatorCookie() {
+  process.env.FORTEXA_AUTH_SECRET = "submit-signed-body-limit-secret";
+  const token = createSessionToken({
+    email: "operator@fortexa.local",
+    role: "operator",
+    userId: "submit-signed-body-limit-operator",
+    expiresInSeconds: 120,
+  });
+
+  return `${AUTH_COOKIE_KEY}=${token}`;
+}
+
+describe("POST /api/stellar/submit-signed body limits", () => {
+  it("returns 413 for oversized JSON payloads", async () => {
+    const padding = "x".repeat(DEFAULT_JSON_BODY_MAX_BYTES);
+    const request = new NextRequest("http://localhost/api/stellar/submit-signed", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        cookie: operatorCookie(),
+      },
+      body: `{"signedXdr":"${padding}"}`,
+    });
+
+    const response = await POST(request);
+    expect(response.status).toBe(413);
+
+    const payload = (await response.json()) as { error: string };
+    expect(payload.error).toContain("byte limit");
+  });
+
+  it("returns validation error for malformed small JSON", async () => {
+    const request = new NextRequest("http://localhost/api/stellar/submit-signed", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        cookie: operatorCookie(),
+      },
+      body: "{not-json",
+    });
+
+    const response = await POST(request);
+    expect(response.status).toBe(400);
+
+    const payload = (await response.json()) as { error: string };
+    expect(payload.error).toBe("Invalid signed transaction submission.");
+  });
+});

src/lib/http/read-json-body.test.ts
@@ -1,0 +1,65 @@
+import { describe, expect, it } from "vitest";
+
+import {
+  DEFAULT_JSON_BODY_MAX_BYTES,
+  getJsonBodyMaxBytes,
+  readJsonBody,
+} from "@/lib/http/read-json-body";
+
+function requestWithBody(body: string, contentLength?: number) {
+  const headers = new Headers({ "content-type": "application/json" });
+  if (contentLength !== undefined) {
+    headers.set("content-length", String(contentLength));
+  }
+
+  return new Request("http://localhost/api/test", {
+    method: "POST",
+    headers,
+    body,
+  });
+}
+
+describe("readJsonBody", () => {
+  it("parses valid JSON within the limit", async () => {
+    const result = await readJsonBody(requestWithBody(JSON.stringify({ ok: true })));
+
+    expect(result).toEqual({ ok: true, data: { ok: true } });
+  });
+
+  it("returns 413 when Content-Length exceeds the limit", async () => {
+    const result = await readJsonBody(
+      requestWithBody("{}", DEFAULT_JSON_BODY_MAX_BYTES + 1),
+      { maxBytes: 1024 },
+    );
+
+    expect(result.ok).toBe(false);
+    if (!result.ok) {
+      expect(result.status).toBe(413);
+    }
+  });
+
+  it("returns 413 when streamed body exceeds the limit", async () => {
+    const oversized = "x".repeat(2048);
+    const result = await readJsonBody(requestWithBody(oversized), { maxBytes: 1024 });
+
+    expect(result.ok).toBe(false);
+    if (!result.ok) {
+      expect(result.status).toBe(413);
+    }
+  });
+
+  it("treats malformed small JSON as an empty object", async () => {
+    const result = await readJsonBody(requestWithBody("{not-json"));
+
+    expect(result).toEqual({ ok: true, data: {} });
+  });
+
+  it("reads FORTEXA_JSON_BODY_MAX_BYTES when set", () => {
+    const previous = process.env.FORTEXA_JSON_BODY_MAX_BYTES;
+    process.env.FORTEXA_JSON_BODY_MAX_BYTES = "8192";
+
+    expect(getJsonBodyMaxBytes()).toBe(8192);
+
+    process.env.FORTEXA_JSON_BODY_MAX_BYTES = previous;
+  });
+});

src/lib/http/read-json-body.ts
@@ -1,0 +1,95 @@
+/** Default max JSON body size for sensitive API routes (64 KiB). */
+export const DEFAULT_JSON_BODY_MAX_BYTES = 64 * 1024;
+
+export function getJsonBodyMaxBytes(): number {
+  const configured = process.env.FORTEXA_JSON_BODY_MAX_BYTES;
+  if (!configured) {
+    return DEFAULT_JSON_BODY_MAX_BYTES;
+  }
+
+  const parsed = Number.parseInt(configured, 10);
+  if (!Number.isFinite(parsed) || parsed <= 0) {
+    return DEFAULT_JSON_BODY_MAX_BYTES;
+  }
+
+  return parsed;
+}
+
+export type ReadJsonBodyResult =
+  | { ok: true; data: unknown }
+  | { ok: false; status: 413; error: string };
+
+export async function readJsonBody(
+  request: Request,
+  options?: { maxBytes?: number },
+): Promise<ReadJsonBodyResult> {
+  const maxBytes = options?.maxBytes ?? getJsonBodyMaxBytes();
+  const readResult = await readBodyTextWithLimit(request, maxBytes);
+
+  if (!readResult.ok) {
+    return {
+      ok: false,
+      status: 413,
+      error: `Request body exceeds the ${maxBytes}-byte limit.`,
+    };
+  }
+
+  return { ok: true, data: parseJsonBodyText(readResult.text) };
+}
+
+async function readBodyTextWithLimit(
+  request: Request,
+  maxBytes: number,
+): Promise<{ ok: true; text: string } | { ok: false }> {
+  const contentLength = request.headers.get("content-length");
+  if (contentLength) {
+    const length = Number.parseInt(contentLength, 10);
+    if (Number.isFinite(length) && length > maxBytes) {
+      return { ok: false };
+    }
+  }
+
+  const body = request.body;
+  if (!body) {
+    return { ok: true, text: "" };
+  }
+
+  const reader = body.getReader();
+  const decoder = new TextDecoder();
+  let bytesRead = 0;
+  let text = "";
+
+  try {
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) {
+        break;
+      }
+
+      bytesRead += value.byteLength;
+      if (bytesRead > maxBytes) {
+        await reader.cancel();
+        return { ok: false };
+      }
+
+      text += decoder.decode(value, { stream: true });
+    }
+  } finally {
+    text += decoder.decode();
+  }
+
+  return { ok: true, text };
+}
+
+function parseJsonBodyText(text: string): unknown {
+  const trimmed = text.trim();
+  if (trimmed === "") {
+    return {};
+  }
+
+  try {
+    return JSON.parse(trimmed) as unknown;
+  } catch {
+    return {};
+  }
+}

Nexusopen-source/nexusguard

src/app/api/stellar/submit-signed/body-limit.test.ts
@@ -1,0 +1,55 @@
+import { describe, expect, it } from "vitest";
+import { NextRequest } from "next/server";
+
+import { POST } from "@/app/api/stellar/submit-signed/route";
+import { AUTH_COOKIE_KEY, createSessionToken } from "@/lib/auth/session";
+import { DEFAULT_JSON_BODY_MAX_BYTES } from "@/lib/http/read-json-body";
+
+function operatorCookie() {
+  process.env.NEXUSGUARD_AUTH_SECRET = "submit-signed-body-limit-secret";
+  const token = createSessionToken({
+    email: "operator@nexusguard.local",
+    role: "operator",
+    userId: "submit-signed-body-limit-operator",
+    expiresInSeconds: 120,
+  });
+
+  return `${AUTH_COOKIE_KEY}=${token}`;
+}
+
+describe("POST /api/stellar/submit-signed body limits", () => {
+  it("returns 413 for oversized JSON payloads", async () => {
+    const padding = "x".repeat(DEFAULT_JSON_BODY_MAX_BYTES);
+    const request = new NextRequest("http://localhost/api/stellar/submit-signed", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        cookie: operatorCookie(),
+      },
+      body: `{"signedXdr":"${padding}"}`,
+    });
+
+    const response = await POST(request);
+    expect(response.status).toBe(413);
+
+    const payload = (await response.json()) as { error: string };
+    expect(payload.error).toContain("byte limit");
+  });
+
+  it("returns validation error for malformed small JSON", async () => {
+    const request = new NextRequest("http://localhost/api/stellar/submit-signed", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        cookie: operatorCookie(),
+      },
+      body: "{not-json",
+    });
+
+    const response = await POST(request);
+    expect(response.status).toBe(400);
+
+    const payload = (await response.json()) as { error: string };
+    expect(payload.error).toBe("Invalid signed transaction submission.");
+  });
+});

src/lib/http/read-json-body.test.ts
@@ -1,0 +1,65 @@
+import { describe, expect, it } from "vitest";
+
+import {
+  DEFAULT_JSON_BODY_MAX_BYTES,
+  getJsonBodyMaxBytes,
+  readJsonBody,
+} from "@/lib/http/read-json-body";
+
+function requestWithBody(body: string, contentLength?: number) {
+  const headers = new Headers({ "content-type": "application/json" });
+  if (contentLength !== undefined) {
+    headers.set("content-length", String(contentLength));
+  }
+
+  return new Request("http://localhost/api/test", {
+    method: "POST",
+    headers,
+    body,
+  });
+}
+
+describe("readJsonBody", () => {
+  it("parses valid JSON within the limit", async () => {
+    const result = await readJsonBody(requestWithBody(JSON.stringify({ ok: true })));
+
+    expect(result).toEqual({ ok: true, data: { ok: true } });
+  });
+
+  it("returns 413 when Content-Length exceeds the limit", async () => {
+    const result = await readJsonBody(
+      requestWithBody("{}", DEFAULT_JSON_BODY_MAX_BYTES + 1),
+      { maxBytes: 1024 },
+    );
+
+    expect(result.ok).toBe(false);
+    if (!result.ok) {
+      expect(result.status).toBe(413);
+    }
+  });
+
+  it("returns 413 when streamed body exceeds the limit", async () => {
+    const oversized = "x".repeat(2048);
+    const result = await readJsonBody(requestWithBody(oversized), { maxBytes: 1024 });
+
+    expect(result.ok).toBe(false);
+    if (!result.ok) {
+      expect(result.status).toBe(413);
+    }
+  });
+
+  it("treats malformed small JSON as an empty object", async () => {
+    const result = await readJsonBody(requestWithBody("{not-json"));
+
+    expect(result).toEqual({ ok: true, data: {} });
+  });
+
+  it("reads NEXUSGUARD_JSON_BODY_MAX_BYTES when set", () => {
+    const previous = process.env.NEXUSGUARD_JSON_BODY_MAX_BYTES;
+    process.env.NEXUSGUARD_JSON_BODY_MAX_BYTES = "8192";
+
+    expect(getJsonBodyMaxBytes()).toBe(8192);
+
+    process.env.NEXUSGUARD_JSON_BODY_MAX_BYTES = previous;
+  });
+});

src/lib/http/read-json-body.ts
@@ -1,0 +1,95 @@
+/** Default max JSON body size for sensitive API routes (64 KiB). */
+export const DEFAULT_JSON_BODY_MAX_BYTES = 64 * 1024;
+
+export function getJsonBodyMaxBytes(): number {
+  const configured = process.env.NEXUSGUARD_JSON_BODY_MAX_BYTES;
+  if (!configured) {
+    return DEFAULT_JSON_BODY_MAX_BYTES;
+  }
+
+  const parsed = Number.parseInt(configured, 10);
+  if (!Number.isFinite(parsed) || parsed <= 0) {
+    return DEFAULT_JSON_BODY_MAX_BYTES;
+  }
+
+  return parsed;
+}
+
+export type ReadJsonBodyResult =
+  | { ok: true; data: unknown }
+  | { ok: false; status: 413; error: string };
+
+export async function readJsonBody(
+  request: Request,
+  options?: { maxBytes?: number },
+): Promise<ReadJsonBodyResult> {
+  const maxBytes = options?.maxBytes ?? getJsonBodyMaxBytes();
+  const readResult = await readBodyTextWithLimit(request, maxBytes);
+
+  if (!readResult.ok) {
+    return {
+      ok: false,
+      status: 413,
+      error: `Request body exceeds the ${maxBytes}-byte limit.`,
+    };
+  }
+
+  return { ok: true, data: parseJsonBodyText(readResult.text) };
+}
+
+async function readBodyTextWithLimit(
+  request: Request,
+  maxBytes: number,
+): Promise<{ ok: true; text: string } | { ok: false }> {
+  const contentLength = request.headers.get("content-length");
+  if (contentLength) {
+    const length = Number.parseInt(contentLength, 10);
+    if (Number.isFinite(length) && length > maxBytes) {
+      return { ok: false };
+    }
+  }
+
+  const body = request.body;
+  if (!body) {
+    return { ok: true, text: "" };
+  }
+
+  const reader = body.getReader();
+  const decoder = new TextDecoder();
+  let bytesRead = 0;
+  let text = "";
+
+  try {
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) {
+        break;
+      }
+
+      bytesRead += value.byteLength;
+      if (bytesRead > maxBytes) {
+        await reader.cancel();
+        return { ok: false };
+      }
+
+      text += decoder.decode(value, { stream: true });
+    }
+  } finally {
+    text += decoder.decode();
+  }
+
+  return { ok: true, text };
+}
+
+function parseJsonBodyText(text: string): unknown {
+  const trimmed = text.trim();
+  if (trimmed === "") {
+    return {};
+  }
+
+  try {
+    return JSON.parse(trimmed) as unknown;
+  } catch {
+    return {};
+  }
+}