samjay8/Fortexa

feat(auth): add session expiration and revocation for wallet mappings (#47) (#71)
* feat(auth): add session expiration and revocation for wallet mappings

Adds 24-hour expiration to wallet sessions (UserWallet) and correctly rejects
expired sessions with 401 Unauthorized. Also adds a DELETE /api/auth/wallet/revoke
endpoint for operators to clear compromised wallet mappings.

Updates STORAGE_MIGRATIONS, documentation, and adds complete unit test coverage.

Resolves #47

* fix(auth): also check wallet expiration in submit-signed route

* fix: remove trailing whitespace in user-wallet-store.ts

src/lib/storage/migrations.ts
@@ -72,4 +72,11 @@ export const STORAGE_MIGRATIONS: SqlMigration[] = [
       );
     `,
   },
+  {
+    id: "004_wallet_expiration",
+    sql: `
+      ALTER TABLE fortexa_wallets
+        ADD COLUMN IF NOT EXISTS expires_at TIMESTAMPTZ;
+    `,
+  },
 ];

src/lib/storage/user-wallet-store.test.ts
@@ -1,0 +1,76 @@
+import * as fs from "node:fs/promises";
+import * as path from "node:path";
+import { describe, it, expect, beforeEach, afterEach } from "vitest";
+
+import { getUserWallet, upsertUserWallet, revokeUserWallet } from "./user-wallet-store";
+import { getFortexaStoreDir } from "./paths";
+
+// Ensure tests use the fallback JSON store by bypassing Postgres if we don't configure it.
+// The existing app logic falls back to JSON if db is unavailable.
+
+describe("user-wallet-store (fallback)", () => {
+  const storePath = path.join(getFortexaStoreDir(), "wallets.json");
+
+  beforeEach(async () => {
+    // ensure empty store before each test
+    await fs.mkdir(getFortexaStoreDir(), { recursive: true }).catch(() => {});
+    await fs.writeFile(storePath, JSON.stringify({ wallets: {} }), "utf8").catch(() => {});
+  });
+
+  afterEach(async () => {
+    await fs.unlink(storePath).catch(() => {});
+  });
+
+  it("returns null for missing mapping", async () => {
+    const wallet = await getUserWallet("missing-user");
+    expect(wallet).toBeNull();
+  });
+
+  it("upserts and returns a valid session mapping", async () => {
+    const wallet = await upsertUserWallet("user-1", {
+      publicKey: "GDEV123",
+      source: "external",
+      provider: "test",
+    });
+
+    expect(wallet.userId).toBe("user-1");
+    expect(wallet.publicKey).toBe("GDEV123");
+    expect(wallet.expiresAt).toBeDefined();
+
+    const fetched = await getUserWallet("user-1");
+    expect(fetched).not.toBeNull();
+    if (fetched && !("expired" in fetched)) {
+      expect(fetched.publicKey).toBe("GDEV123");
+    } else {
+      expect.fail("Expected a valid UserWallet");
+    }
+  });
+
+  it("identifies expired session mappings", async () => {
+    // Insert with expiration in the past
+    await upsertUserWallet("user-2", {
+      publicKey: "GDEV456",
+      source: "external",
+      expiresAt: new Date(Date.now() - 1000).toISOString(),
+    });
+
+    const fetched = await getUserWallet("user-2");
+    expect(fetched).toEqual({ expired: true });
+  });
+
+  it("revokes session mappings deterministically", async () => {
+    await upsertUserWallet("user-3", {
+      publicKey: "GDEV789",
+      source: "external",
+    });
+
+    let fetched = await getUserWallet("user-3");
+    expect(fetched).not.toBeNull();
+    expect(fetched).not.toEqual({ expired: true });
+
+    await revokeUserWallet("user-3");
+
+    fetched = await getUserWallet("user-3");
+    expect(fetched).toBeNull();
+  });
+});

src/lib/storage/user-wallet-store.ts
@@ -10,6 +10,7 @@ export type UserWallet = {
   provider?: string;
   createdAt: string;
   updatedAt: string;
+  expiresAt?: string;
 };
 
 type WalletStoreFile = {
@@ -86,7 +87,7 @@ async function writeStore(store: WalletStoreFile) {
   await fs.writeFile(storePath, JSON.stringify(store, null, 2), "utf8");
 }
 
-export async function getUserWallet(userId: string) {
+export async function getUserWallet(userId: string): Promise<UserWallet | { expired: true } | null> {
   const db = await runWithDatabase("getUserWallet", async (pool) => {
     const result = await pool.query<{
       user_id: string;
@@ -95,9 +96,10 @@ export async function getUserWallet(userId: string) {
       provider: string | null;
       created_at: string;
       updated_at: string;
+      expires_at: string | null;
     }>(
       `
-        SELECT user_id, public_key, source, provider, created_at, updated_at
+        SELECT user_id, public_key, source, provider, created_at, updated_at, expires_at
         FROM fortexa_wallets
         WHERE user_id = $1
       `,
@@ -109,6 +111,10 @@ export async function getUserWallet(userId: string) {
       return null;
     }
 
+    if (row.expires_at && new Date(row.expires_at).getTime() < Date.now()) {
+      return { expired: true as const };
+    }
+
     return {
       userId: row.user_id,
       publicKey: row.public_key,
@@ -116,6 +122,7 @@ export async function getUserWallet(userId: string) {
       provider: row.provider ?? undefined,
       createdAt: new Date(row.created_at).toISOString(),
       updatedAt: new Date(row.updated_at).toISOString(),
+      expiresAt: row.expires_at ? new Date(row.expires_at).toISOString() : undefined,
     };
   });
 
@@ -128,7 +135,11 @@ export async function getUserWallet(userId: string) {
   if (!wallet || typeof wallet !== "object" || !("source" in wallet) || !("publicKey" in wallet)) {
     return null;
   }
-  return wallet as UserWallet;
+  const userWallet = wallet as UserWallet;
+  if (userWallet.expiresAt && new Date(userWallet.expiresAt).getTime() < Date.now()) {
+    return { expired: true as const };
+  }
+  return userWallet;
 }
 
 export async function upsertUserWallet(
@@ -137,6 +148,7 @@ export async function upsertUserWallet(
     publicKey: string;
     source: "external";
     provider?: string;
+    expiresAt?: string;
   }
 ) {
   const db = await runWithDatabase("upsertUserWallet", async (pool) => {
@@ -153,19 +165,22 @@ export async function upsertUserWallet(
     const createdAt = existing.rows[0]?.created_at
       ? new Date(existing.rows[0].created_at).toISOString()
       : nowIso;
+    // Default expiration to 24 hours from now if not provided
+    const expiresAt = payload.expiresAt ?? new Date(Date.now() + 24 * 60 * 60 * 1000).toISOString();
 
     await pool.query(
       `
-        INSERT INTO fortexa_wallets (user_id, public_key, source, provider, created_at, updated_at)
-        VALUES ($1, $2, $3, $4, $5::timestamptz, $6::timestamptz)
+        INSERT INTO fortexa_wallets (user_id, public_key, source, provider, created_at, updated_at, expires_at)
+        VALUES ($1, $2, $3, $4, $5::timestamptz, $6::timestamptz, $7::timestamptz)
         ON CONFLICT (user_id)
         DO UPDATE SET
           public_key = EXCLUDED.public_key,
           source = EXCLUDED.source,
           provider = EXCLUDED.provider,
-          updated_at = EXCLUDED.updated_at
+          updated_at = EXCLUDED.updated_at,
+          expires_at = EXCLUDED.expires_at
       `,
-      [userId, payload.publicKey, payload.source, payload.provider ?? null, createdAt, nowIso]
+      [userId, payload.publicKey, payload.source, payload.provider ?? null, createdAt, nowIso, expiresAt]
     );
 
     return {
@@ -175,6 +190,7 @@ export async function upsertUserWallet(
       provider: payload.provider,
       createdAt,
       updatedAt: nowIso,
+      expiresAt,
     };
   });
 
@@ -185,17 +201,43 @@ export async function upsertUserWallet(
   const store = await readStore();
   const now = new Date().toISOString();
   const existing = await getUserWallet(userId);
+  const createdAt = (existing && !("expired" in existing)) ? existing.createdAt : now;
+  const expiresAt = payload.expiresAt ?? new Date(Date.now() + 24 * 60 * 60 * 1000).toISOString();
 
   const next: UserWallet = {
     userId,
     publicKey: payload.publicKey,
     source: payload.source,
     provider: payload.provider,
-    createdAt: existing?.createdAt ?? now,
+    createdAt: createdAt,
     updatedAt: now,
+    expiresAt,
   };
 
   store.wallets[userId] = next;
   await writeStore(store);
   return next;
+}
+
+export async function revokeUserWallet(userId: string): Promise<void> {
+  const db = await runWithDatabase("revokeUserWallet", async (pool) => {
+    await pool.query(
+      `
+        DELETE FROM fortexa_wallets
+        WHERE user_id = $1
+      `,
+      [userId]
+    );
+    return true;
+  });
+
+  if (db.available) {
+    return;
+  }
+
+  const store = await readStore();
+  if (store.wallets[userId]) {
+    delete store.wallets[userId];
+    await writeStore(store);
+  }
 }

Nexusopen-source/nexusguard

src/lib/storage/migrations.ts
@@ -72,4 +72,11 @@ export const STORAGE_MIGRATIONS: SqlMigration[] = [
       );
     `,
   },
+  {
+    id: "004_wallet_expiration",
+    sql: `
+      ALTER TABLE nexusguard_wallets
+        ADD COLUMN IF NOT EXISTS expires_at TIMESTAMPTZ;
+    `,
+  },
 ];

src/lib/storage/user-wallet-store.test.ts
@@ -1,0 +1,76 @@
+import * as fs from "node:fs/promises";
+import * as path from "node:path";
+import { describe, it, expect, beforeEach, afterEach } from "vitest";
+
+import { getUserWallet, upsertUserWallet, revokeUserWallet } from "./user-wallet-store";
+import { getNexusGuardStoreDir } from "./paths";
+
+// Ensure tests use the fallback JSON store by bypassing Postgres if we don't configure it.
+// The existing app logic falls back to JSON if db is unavailable.
+
+describe("user-wallet-store (fallback)", () => {
+  const storePath = path.join(getNexusGuardStoreDir(), "wallets.json");
+
+  beforeEach(async () => {
+    // ensure empty store before each test
+    await fs.mkdir(getNexusGuardStoreDir(), { recursive: true }).catch(() => {});
+    await fs.writeFile(storePath, JSON.stringify({ wallets: {} }), "utf8").catch(() => {});
+  });
+
+  afterEach(async () => {
+    await fs.unlink(storePath).catch(() => {});
+  });
+
+  it("returns null for missing mapping", async () => {
+    const wallet = await getUserWallet("missing-user");
+    expect(wallet).toBeNull();
+  });
+
+  it("upserts and returns a valid session mapping", async () => {
+    const wallet = await upsertUserWallet("user-1", {
+      publicKey: "GDEV123",
+      source: "external",
+      provider: "test",
+    });
+
+    expect(wallet.userId).toBe("user-1");
+    expect(wallet.publicKey).toBe("GDEV123");
+    expect(wallet.expiresAt).toBeDefined();
+
+    const fetched = await getUserWallet("user-1");
+    expect(fetched).not.toBeNull();
+    if (fetched && !("expired" in fetched)) {
+      expect(fetched.publicKey).toBe("GDEV123");
+    } else {
+      expect.fail("Expected a valid UserWallet");
+    }
+  });
+
+  it("identifies expired session mappings", async () => {
+    // Insert with expiration in the past
+    await upsertUserWallet("user-2", {
+      publicKey: "GDEV456",
+      source: "external",
+      expiresAt: new Date(Date.now() - 1000).toISOString(),
+    });
+
+    const fetched = await getUserWallet("user-2");
+    expect(fetched).toEqual({ expired: true });
+  });
+
+  it("revokes session mappings deterministically", async () => {
+    await upsertUserWallet("user-3", {
+      publicKey: "GDEV789",
+      source: "external",
+    });
+
+    let fetched = await getUserWallet("user-3");
+    expect(fetched).not.toBeNull();
+    expect(fetched).not.toEqual({ expired: true });
+
+    await revokeUserWallet("user-3");
+
+    fetched = await getUserWallet("user-3");
+    expect(fetched).toBeNull();
+  });
+});

src/lib/storage/user-wallet-store.ts
@@ -10,6 +10,7 @@ export type UserWallet = {
   provider?: string;
   createdAt: string;
   updatedAt: string;
+  expiresAt?: string;
 };
 
 type WalletStoreFile = {
@@ -86,7 +87,7 @@ async function writeStore(store: WalletStoreFile) {
   await fs.writeFile(storePath, JSON.stringify(store, null, 2), "utf8");
 }
 
-export async function getUserWallet(userId: string) {
+export async function getUserWallet(userId: string): Promise<UserWallet | { expired: true } | null> {
   const db = await runWithDatabase("getUserWallet", async (pool) => {
     const result = await pool.query<{
       user_id: string;
@@ -95,9 +96,10 @@ export async function getUserWallet(userId: string) {
       provider: string | null;
       created_at: string;
       updated_at: string;
+      expires_at: string | null;
     }>(
       `
-        SELECT user_id, public_key, source, provider, created_at, updated_at
+        SELECT user_id, public_key, source, provider, created_at, updated_at, expires_at
         FROM nexusguard_wallets
         WHERE user_id = $1
       `,
@@ -109,6 +111,10 @@ export async function getUserWallet(userId: string) {
       return null;
     }
 
+    if (row.expires_at && new Date(row.expires_at).getTime() < Date.now()) {
+      return { expired: true as const };
+    }
+
     return {
       userId: row.user_id,
       publicKey: row.public_key,
@@ -116,6 +122,7 @@ export async function getUserWallet(userId: string) {
       provider: row.provider ?? undefined,
       createdAt: new Date(row.created_at).toISOString(),
       updatedAt: new Date(row.updated_at).toISOString(),
+      expiresAt: row.expires_at ? new Date(row.expires_at).toISOString() : undefined,
     };
   });
 
@@ -128,7 +135,11 @@ export async function getUserWallet(userId: string) {
   if (!wallet || typeof wallet !== "object" || !("source" in wallet) || !("publicKey" in wallet)) {
     return null;
   }
-  return wallet as UserWallet;
+  const userWallet = wallet as UserWallet;
+  if (userWallet.expiresAt && new Date(userWallet.expiresAt).getTime() < Date.now()) {
+    return { expired: true as const };
+  }
+  return userWallet;
 }
 
 export async function upsertUserWallet(
@@ -137,6 +148,7 @@ export async function upsertUserWallet(
     publicKey: string;
     source: "external";
     provider?: string;
+    expiresAt?: string;
   }
 ) {
   const db = await runWithDatabase("upsertUserWallet", async (pool) => {
@@ -153,19 +165,22 @@ export async function upsertUserWallet(
     const createdAt = existing.rows[0]?.created_at
       ? new Date(existing.rows[0].created_at).toISOString()
       : nowIso;
+    // Default expiration to 24 hours from now if not provided
+    const expiresAt = payload.expiresAt ?? new Date(Date.now() + 24 * 60 * 60 * 1000).toISOString();
 
     await pool.query(
       `
-        INSERT INTO nexusguard_wallets (user_id, public_key, source, provider, created_at, updated_at)
-        VALUES ($1, $2, $3, $4, $5::timestamptz, $6::timestamptz)
+        INSERT INTO nexusguard_wallets (user_id, public_key, source, provider, created_at, updated_at, expires_at)
+        VALUES ($1, $2, $3, $4, $5::timestamptz, $6::timestamptz, $7::timestamptz)
         ON CONFLICT (user_id)
         DO UPDATE SET
           public_key = EXCLUDED.public_key,
           source = EXCLUDED.source,
           provider = EXCLUDED.provider,
-          updated_at = EXCLUDED.updated_at
+          updated_at = EXCLUDED.updated_at,
+          expires_at = EXCLUDED.expires_at
       `,
-      [userId, payload.publicKey, payload.source, payload.provider ?? null, createdAt, nowIso]
+      [userId, payload.publicKey, payload.source, payload.provider ?? null, createdAt, nowIso, expiresAt]
     );
 
     return {
@@ -175,6 +190,7 @@ export async function upsertUserWallet(
       provider: payload.provider,
       createdAt,
       updatedAt: nowIso,
+      expiresAt,
     };
   });
 
@@ -185,17 +201,43 @@ export async function upsertUserWallet(
   const store = await readStore();
   const now = new Date().toISOString();
   const existing = await getUserWallet(userId);
+  const createdAt = (existing && !("expired" in existing)) ? existing.createdAt : now;
+  const expiresAt = payload.expiresAt ?? new Date(Date.now() + 24 * 60 * 60 * 1000).toISOString();
 
   const next: UserWallet = {
     userId,
     publicKey: payload.publicKey,
     source: payload.source,
     provider: payload.provider,
-    createdAt: existing?.createdAt ?? now,
+    createdAt: createdAt,
     updatedAt: now,
+    expiresAt,
   };
 
   store.wallets[userId] = next;
   await writeStore(store);
   return next;
+}
+
+export async function revokeUserWallet(userId: string): Promise<void> {
+  const db = await runWithDatabase("revokeUserWallet", async (pool) => {
+    await pool.query(
+      `
+        DELETE FROM nexusguard_wallets
+        WHERE user_id = $1
+      `,
+      [userId]
+    );
+    return true;
+  });
+
+  if (db.available) {
+    return;
+  }
+
+  const store = await readStore();
+  if (store.wallets[userId]) {
+    delete store.wallets[userId];
+    await writeStore(store);
+  }
 }