samjay8/Fortexa

docs: add API authorization matrix and high-risk route auth tests (#89)
- Add docs/api-authz-matrix.md: single source of truth listing every API
  route with its access level, unauthenticated response, viewer behavior,
  and whether the route is state-changing
- Add /api/audit route.test.ts covering unauthenticated (401), operator
  (200), and viewer (200) access
- Extend /api/decision tests with viewer-rejection case (403)
- Extend /api/policy tests with viewer read (200) and viewer write (403)
- Extend /api/stellar/build-payment tests with unauthenticated (401) and
  viewer-rejection (403) cases
- Extend /api/stellar/submit-signed tests with unauthenticated (401) and
  viewer-rejection (403) cases

src/app/api/audit/route.test.ts
@@ -1,0 +1,66 @@
+import { describe, expect, it } from "vitest";
+import { NextRequest } from "next/server";
+
+import { AUTH_COOKIE_KEY, createSessionToken } from "@/lib/auth/session";
+import { GET } from "@/app/api/audit/route";
+
+function operatorCookie() {
+  process.env.FORTEXA_AUTH_SECRET = "integration-test-secret";
+  const token = createSessionToken({
+    email: "operator@fortexa.local",
+    role: "operator",
+    userId: "audit-operator-id",
+    expiresInSeconds: 120,
+  });
+
+  return `${AUTH_COOKIE_KEY}=${token}`;
+}
+
+function viewerCookie() {
+  process.env.FORTEXA_AUTH_SECRET = "integration-test-secret";
+  const token = createSessionToken({
+    email: "viewer@fortexa.local",
+    role: "viewer",
+    userId: "audit-viewer-id",
+    expiresInSeconds: 120,
+  });
+
+  return `${AUTH_COOKIE_KEY}=${token}`;
+}
+
+describe("/api/audit route authorization", () => {
+  it("returns 401 when unauthenticated", async () => {
+    const request = new NextRequest("http://localhost/api/audit", { method: "GET" });
+    const response = await GET(request);
+
+    expect(response.status).toBe(401);
+  });
+
+  it("returns 200 for operator", async () => {
+    const request = new NextRequest("http://localhost/api/audit", {
+      method: "GET",
+      headers: { cookie: operatorCookie() },
+    });
+    const response = await GET(request);
+
+    expect(response.status).toBe(200);
+
+    const payload = (await response.json()) as { entries: unknown[]; userId: string };
+    expect(Array.isArray(payload.entries)).toBe(true);
+    expect(payload.userId).toBe("audit-operator-id");
+  });
+
+  it("returns 200 for viewer (read-only access allowed)", async () => {
+    const request = new NextRequest("http://localhost/api/audit", {
+      method: "GET",
+      headers: { cookie: viewerCookie() },
+    });
+    const response = await GET(request);
+
+    expect(response.status).toBe(200);
+
+    const payload = (await response.json()) as { entries: unknown[]; userId: string };
+    expect(Array.isArray(payload.entries)).toBe(true);
+    expect(payload.userId).toBe("audit-viewer-id");
+  });
+});

src/app/api/decision/route.test.ts
@@ -16,6 +16,18 @@ function operatorCookie() {
   return `${AUTH_COOKIE_KEY}=${token}`;
 }
 
+function viewerCookie() {
+  process.env.FORTEXA_AUTH_SECRET = "integration-test-secret";
+  const token = createSessionToken({
+    email: "viewer@fortexa.local",
+    role: "viewer",
+    userId: "decision-viewer-id",
+    expiresInSeconds: 120,
+  });
+
+  return `${AUTH_COOKIE_KEY}=${token}`;
+}
+
 describe("/api/decision route", () => {
   it("returns 401 when unauthenticated", async () => {
     const request = new NextRequest("http://localhost/api/decision", {
@@ -26,6 +38,20 @@ describe("/api/decision route", () => {
 
     const response = await POST(request);
     expect(response.status).toBe(401);
+  });
+
+  it("returns 403 for viewer role (operator-only route)", async () => {
+    const request = new NextRequest("http://localhost/api/decision", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        cookie: viewerCookie(),
+      },
+      body: JSON.stringify({ scenarioId: "safe-research-payment" }),
+    });
+
+    const response = await POST(request);
+    expect(response.status).toBe(403);
   });
 
   it("evaluates scenario for operator", async () => {

src/app/api/policy/route.test.ts
@@ -16,12 +16,61 @@ function operatorCookie() {
   return `${AUTH_COOKIE_KEY}=${token}`;
 }
 
+function viewerCookie() {
+  process.env.FORTEXA_AUTH_SECRET = "integration-test-secret";
+  const token = createSessionToken({
+    email: "viewer@fortexa.local",
+    role: "viewer",
+    userId: "viewer-user-id",
+    expiresInSeconds: 120,
+  });
+
+  return `${AUTH_COOKIE_KEY}=${token}`;
+}
+
 describe("/api/policy route", () => {
   it("returns 401 when unauthenticated", async () => {
     const request = new NextRequest("http://localhost/api/policy", { method: "GET" });
     const response = await GET(request);
 
     expect(response.status).toBe(401);
+  });
+
+  it("returns 200 for viewer reading policy (read access allowed)", async () => {
+    const request = new NextRequest("http://localhost/api/policy", {
+      method: "GET",
+      headers: { cookie: viewerCookie() },
+    });
+    const response = await GET(request);
+
+    expect(response.status).toBe(200);
+
+    const payload = (await response.json()) as { policy: unknown };
+    expect(payload.policy).toBeDefined();
+  });
+
+  it("returns 403 for viewer attempting policy update (operator-only)", async () => {
+    const request = new NextRequest("http://localhost/api/policy", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        cookie: viewerCookie(),
+      },
+      body: JSON.stringify({
+        allowedDomains: [],
+        blockedDomains: [],
+        allowedTools: [],
+        blockedTools: [],
+        perTxCapXLM: 100,
+        dailyCapXLM: 500,
+        maxToolCallsPerDay: 10,
+        riskThreshold: 70,
+        allowedHours: { start: 0, end: 23 },
+      }),
+    });
+    const response = await POST(request);
+
+    expect(response.status).toBe(403);
   });
 
   it("allows operator to update and read policy", async () => {

src/app/api/stellar/build-payment/route.test.ts
@@ -261,3 +261,55 @@ describe("POST /api/stellar/build-payment quote verification", () => {
     expect(payload.error).toBe("Invalid payment build request.");
   });
 });
+
+function viewerCookie() {
+  const token = createSessionToken({
+    email: "viewer@fortexa.local",
+    role: "viewer",
+    userId: "build-payment-viewer",
+    expiresInSeconds: 120,
+  });
+
+  return `${AUTH_COOKIE_KEY}=${token}`;
+}
+
+describe("POST /api/stellar/build-payment authorization", () => {
+  it("returns 401 when unauthenticated", async () => {
+    const request = new NextRequest("http://localhost/api/stellar/build-payment", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        auditEntryId: "any-id",
+        destination: destinationKeypair.publicKey(),
+        amountXLM: "10.0000000",
+        asset: "native",
+        memo: "fortexa:test",
+        network: "testnet",
+      }),
+    });
+
+    const response = await buildPaymentPost(request);
+    expect(response.status).toBe(401);
+  });
+
+  it("returns 403 for viewer role (operator-only route)", async () => {
+    const request = new NextRequest("http://localhost/api/stellar/build-payment", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        cookie: viewerCookie(),
+      },
+      body: JSON.stringify({
+        auditEntryId: "any-id",
+        destination: destinationKeypair.publicKey(),
+        amountXLM: "10.0000000",
+        asset: "native",
+        memo: "fortexa:test",
+        network: "testnet",
+      }),
+    });
+
+    const response = await buildPaymentPost(request);
+    expect(response.status).toBe(403);
+  });
+});

src/app/api/stellar/submit-signed/route.test.ts
@@ -1,6 +1,8 @@
 import { describe, expect, it } from "vitest";
+import { NextRequest } from "next/server";
 
-import { formatSubmitError } from "./route";
+import { AUTH_COOKIE_KEY, createSessionToken } from "@/lib/auth/session";
+import { formatSubmitError, POST } from "./route";
 
 function mockHorizonError(txCode: string, opCodes?: string[]) {
   const error = new Error("Request failed with status code 400") as Error & {
@@ -66,5 +68,48 @@ describe("formatSubmitError - Horizon error catalog", () => {
     expect(formatted.opCodes).toEqual(["op_unknown"]);
     expect(formatted.explanation).toBeUndefined();
     expect(formatted.nextStep).toBeUndefined();
+  });
+});
+
+function setupSecret() {
+  process.env.FORTEXA_AUTH_SECRET = "integration-test-secret";
+}
+
+function viewerCookie() {
+  setupSecret();
+  const token = createSessionToken({
+    email: "viewer@fortexa.local",
+    role: "viewer",
+    userId: "submit-viewer-id",
+    expiresInSeconds: 120,
+  });
+
+  return `${AUTH_COOKIE_KEY}=${token}`;
+}
+
+describe("POST /api/stellar/submit-signed authorization", () => {
+  it("returns 401 when unauthenticated", async () => {
+    const request = new NextRequest("http://localhost/api/stellar/submit-signed", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({ signedXdr: "AAAA" }),
+    });
+
+    const response = await POST(request);
+    expect(response.status).toBe(401);
+  });
+
+  it("returns 403 for viewer role (operator-only route)", async () => {
+    const request = new NextRequest("http://localhost/api/stellar/submit-signed", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        cookie: viewerCookie(),
+      },
+      body: JSON.stringify({ signedXdr: "AAAA" }),
+    });
+
+    const response = await POST(request);
+    expect(response.status).toBe(403);
   });
 });

Nexusopen-source/nexusguard

src/app/api/audit/route.test.ts
@@ -1,0 +1,66 @@
+import { describe, expect, it } from "vitest";
+import { NextRequest } from "next/server";
+
+import { AUTH_COOKIE_KEY, createSessionToken } from "@/lib/auth/session";
+import { GET } from "@/app/api/audit/route";
+
+function operatorCookie() {
+  process.env.NEXUSGUARD_AUTH_SECRET = "integration-test-secret";
+  const token = createSessionToken({
+    email: "operator@nexusguard.local",
+    role: "operator",
+    userId: "audit-operator-id",
+    expiresInSeconds: 120,
+  });
+
+  return `${AUTH_COOKIE_KEY}=${token}`;
+}
+
+function viewerCookie() {
+  process.env.NEXUSGUARD_AUTH_SECRET = "integration-test-secret";
+  const token = createSessionToken({
+    email: "viewer@nexusguard.local",
+    role: "viewer",
+    userId: "audit-viewer-id",
+    expiresInSeconds: 120,
+  });
+
+  return `${AUTH_COOKIE_KEY}=${token}`;
+}
+
+describe("/api/audit route authorization", () => {
+  it("returns 401 when unauthenticated", async () => {
+    const request = new NextRequest("http://localhost/api/audit", { method: "GET" });
+    const response = await GET(request);
+
+    expect(response.status).toBe(401);
+  });
+
+  it("returns 200 for operator", async () => {
+    const request = new NextRequest("http://localhost/api/audit", {
+      method: "GET",
+      headers: { cookie: operatorCookie() },
+    });
+    const response = await GET(request);
+
+    expect(response.status).toBe(200);
+
+    const payload = (await response.json()) as { entries: unknown[]; userId: string };
+    expect(Array.isArray(payload.entries)).toBe(true);
+    expect(payload.userId).toBe("audit-operator-id");
+  });
+
+  it("returns 200 for viewer (read-only access allowed)", async () => {
+    const request = new NextRequest("http://localhost/api/audit", {
+      method: "GET",
+      headers: { cookie: viewerCookie() },
+    });
+    const response = await GET(request);
+
+    expect(response.status).toBe(200);
+
+    const payload = (await response.json()) as { entries: unknown[]; userId: string };
+    expect(Array.isArray(payload.entries)).toBe(true);
+    expect(payload.userId).toBe("audit-viewer-id");
+  });
+});

src/app/api/decision/route.test.ts
@@ -16,6 +16,18 @@ function operatorCookie() {
   return `${AUTH_COOKIE_KEY}=${token}`;
 }
 
+function viewerCookie() {
+  process.env.NEXUSGUARD_AUTH_SECRET = "integration-test-secret";
+  const token = createSessionToken({
+    email: "viewer@nexusguard.local",
+    role: "viewer",
+    userId: "decision-viewer-id",
+    expiresInSeconds: 120,
+  });
+
+  return `${AUTH_COOKIE_KEY}=${token}`;
+}
+
 describe("/api/decision route", () => {
   it("returns 401 when unauthenticated", async () => {
     const request = new NextRequest("http://localhost/api/decision", {
@@ -26,6 +38,20 @@ describe("/api/decision route", () => {
 
     const response = await POST(request);
     expect(response.status).toBe(401);
+  });
+
+  it("returns 403 for viewer role (operator-only route)", async () => {
+    const request = new NextRequest("http://localhost/api/decision", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        cookie: viewerCookie(),
+      },
+      body: JSON.stringify({ scenarioId: "safe-research-payment" }),
+    });
+
+    const response = await POST(request);
+    expect(response.status).toBe(403);
   });
 
   it("evaluates scenario for operator", async () => {

src/app/api/policy/route.test.ts
@@ -16,12 +16,61 @@ function operatorCookie() {
   return `${AUTH_COOKIE_KEY}=${token}`;
 }
 
+function viewerCookie() {
+  process.env.NEXUSGUARD_AUTH_SECRET = "integration-test-secret";
+  const token = createSessionToken({
+    email: "viewer@nexusguard.local",
+    role: "viewer",
+    userId: "viewer-user-id",
+    expiresInSeconds: 120,
+  });
+
+  return `${AUTH_COOKIE_KEY}=${token}`;
+}
+
 describe("/api/policy route", () => {
   it("returns 401 when unauthenticated", async () => {
     const request = new NextRequest("http://localhost/api/policy", { method: "GET" });
     const response = await GET(request);
 
     expect(response.status).toBe(401);
+  });
+
+  it("returns 200 for viewer reading policy (read access allowed)", async () => {
+    const request = new NextRequest("http://localhost/api/policy", {
+      method: "GET",
+      headers: { cookie: viewerCookie() },
+    });
+    const response = await GET(request);
+
+    expect(response.status).toBe(200);
+
+    const payload = (await response.json()) as { policy: unknown };
+    expect(payload.policy).toBeDefined();
+  });
+
+  it("returns 403 for viewer attempting policy update (operator-only)", async () => {
+    const request = new NextRequest("http://localhost/api/policy", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        cookie: viewerCookie(),
+      },
+      body: JSON.stringify({
+        allowedDomains: [],
+        blockedDomains: [],
+        allowedTools: [],
+        blockedTools: [],
+        perTxCapXLM: 100,
+        dailyCapXLM: 500,
+        maxToolCallsPerDay: 10,
+        riskThreshold: 70,
+        allowedHours: { start: 0, end: 23 },
+      }),
+    });
+    const response = await POST(request);
+
+    expect(response.status).toBe(403);
   });
 
   it("allows operator to update and read policy", async () => {

src/app/api/stellar/build-payment/route.test.ts
@@ -261,3 +261,55 @@ describe("POST /api/stellar/build-payment quote verification", () => {
     expect(payload.error).toBe("Invalid payment build request.");
   });
 });
+
+function viewerCookie() {
+  const token = createSessionToken({
+    email: "viewer@nexusguard.local",
+    role: "viewer",
+    userId: "build-payment-viewer",
+    expiresInSeconds: 120,
+  });
+
+  return `${AUTH_COOKIE_KEY}=${token}`;
+}
+
+describe("POST /api/stellar/build-payment authorization", () => {
+  it("returns 401 when unauthenticated", async () => {
+    const request = new NextRequest("http://localhost/api/stellar/build-payment", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        auditEntryId: "any-id",
+        destination: destinationKeypair.publicKey(),
+        amountXLM: "10.0000000",
+        asset: "native",
+        memo: "nexusguard:test",
+        network: "testnet",
+      }),
+    });
+
+    const response = await buildPaymentPost(request);
+    expect(response.status).toBe(401);
+  });
+
+  it("returns 403 for viewer role (operator-only route)", async () => {
+    const request = new NextRequest("http://localhost/api/stellar/build-payment", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        cookie: viewerCookie(),
+      },
+      body: JSON.stringify({
+        auditEntryId: "any-id",
+        destination: destinationKeypair.publicKey(),
+        amountXLM: "10.0000000",
+        asset: "native",
+        memo: "nexusguard:test",
+        network: "testnet",
+      }),
+    });
+
+    const response = await buildPaymentPost(request);
+    expect(response.status).toBe(403);
+  });
+});

src/app/api/stellar/submit-signed/route.test.ts
@@ -1,6 +1,8 @@
 import { describe, expect, it } from "vitest";
+import { NextRequest } from "next/server";
 
-import { formatSubmitError } from "./route";
+import { AUTH_COOKIE_KEY, createSessionToken } from "@/lib/auth/session";
+import { formatSubmitError, POST } from "./route";
 
 function mockHorizonError(txCode: string, opCodes?: string[]) {
   const error = new Error("Request failed with status code 400") as Error & {
@@ -66,5 +68,48 @@ describe("formatSubmitError - Horizon error catalog", () => {
     expect(formatted.opCodes).toEqual(["op_unknown"]);
     expect(formatted.explanation).toBeUndefined();
     expect(formatted.nextStep).toBeUndefined();
+  });
+});
+
+function setupSecret() {
+  process.env.NEXUSGUARD_AUTH_SECRET = "integration-test-secret";
+}
+
+function viewerCookie() {
+  setupSecret();
+  const token = createSessionToken({
+    email: "viewer@nexusguard.local",
+    role: "viewer",
+    userId: "submit-viewer-id",
+    expiresInSeconds: 120,
+  });
+
+  return `${AUTH_COOKIE_KEY}=${token}`;
+}
+
+describe("POST /api/stellar/submit-signed authorization", () => {
+  it("returns 401 when unauthenticated", async () => {
+    const request = new NextRequest("http://localhost/api/stellar/submit-signed", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({ signedXdr: "AAAA" }),
+    });
+
+    const response = await POST(request);
+    expect(response.status).toBe(401);
+  });
+
+  it("returns 403 for viewer role (operator-only route)", async () => {
+    const request = new NextRequest("http://localhost/api/stellar/submit-signed", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        cookie: viewerCookie(),
+      },
+      body: JSON.stringify({ signedXdr: "AAAA" }),
+    });
+
+    const response = await POST(request);
+    expect(response.status).toBe(403);
   });
 });