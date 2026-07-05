samjay8/Fortexa

feat(metrics): add decision outcome and Stellar submit counters (#88)

src/lib/observability/metrics.test.ts
@@ -1,6 +1,15 @@
 import { beforeEach, describe, expect, it } from "vitest";
 
-import { getMetricsSnapshot, recordApiMetric, resetMetrics, toPrometheusText } from "@/lib/observability/metrics";
+import {
+  getDecisionOutcomeCounts,
+  getMetricsSnapshot,
+  getStellarSubmitResultCounts,
+  recordApiMetric,
+  recordDecisionOutcome,
+  recordStellarSubmitResult,
+  resetMetrics,
+  toPrometheusText,
+} from "@/lib/observability/metrics";
 
 describe("observability metrics", () => {
   beforeEach(() => {
@@ -28,5 +37,66 @@ describe("observability metrics", () => {
 
     expect(output).toContain("fortexa_requests_total");
     expect(output).toContain('route="/api/policy"');
+  });
+
+  it("increments decision outcome counters (success path)", () => {
+    recordDecisionOutcome("APPROVE");
+    recordDecisionOutcome("APPROVE");
+    recordDecisionOutcome("WARN");
+
+    const counts = getDecisionOutcomeCounts();
+    expect(counts.get("APPROVE")).toBe(2);
+    expect(counts.get("WARN")).toBe(1);
+    expect(counts.get("REQUIRE_APPROVAL")).toBeUndefined();
+    expect(counts.get("BLOCK")).toBeUndefined();
+  });
+
+  it("increments decision outcome counters (failure path)", () => {
+    recordDecisionOutcome("BLOCK");
+
+    const counts = getDecisionOutcomeCounts();
+    expect(counts.get("BLOCK")).toBe(1);
+    expect(counts.get("APPROVE")).toBeUndefined();
+  });
+
+  it("increments stellar submit result counters (success path)", () => {
+    recordStellarSubmitResult("success");
+    recordStellarSubmitResult("success");
+    recordStellarSubmitResult("idempotency_replay");
+
+    const counts = getStellarSubmitResultCounts();
+    expect(counts.get("success")).toBe(2);
+    expect(counts.get("idempotency_replay")).toBe(1);
+    expect(counts.get("horizon_failure")).toBeUndefined();
+  });
+
+  it("increments stellar submit result counters (failure path)", () => {
+    recordStellarSubmitResult("horizon_failure");
+    recordStellarSubmitResult("idempotency_conflict");
+
+    const counts = getStellarSubmitResultCounts();
+    expect(counts.get("horizon_failure")).toBe(1);
+    expect(counts.get("idempotency_conflict")).toBe(1);
+    expect(counts.get("success")).toBeUndefined();
+  });
+
+  it("includes new counters in prometheus text output", () => {
+    recordDecisionOutcome("APPROVE");
+    recordStellarSubmitResult("success");
+
+    const output = toPrometheusText();
+    expect(output).toContain("fortexa_decision_outcomes_total");
+    expect(output).toContain('outcome="APPROVE"');
+    expect(output).toContain("fortexa_stellar_submit_results_total");
+    expect(output).toContain('result="success"');
+  });
+
+  it("resets new counters alongside existing buckets", () => {
+    recordDecisionOutcome("WARN");
+    recordStellarSubmitResult("success");
+    resetMetrics();
+
+    expect(getDecisionOutcomeCounts().size).toBe(0);
+    expect(getStellarSubmitResultCounts().size).toBe(0);
   });
 });

src/lib/observability/metrics.ts
@@ -1,5 +1,16 @@
 type MetricKey = `${string}:${string}`;
 
+export type DecisionOutcome = "APPROVE" | "WARN" | "REQUIRE_APPROVAL" | "BLOCK";
+export type StellarSubmitResult =
+  | "success"
+  | "horizon_failure"
+  | "validation_failure"
+  | "idempotency_replay"
+  | "idempotency_conflict";
+
+const decisionOutcomeCounts = new Map<DecisionOutcome, number>();
+const stellarSubmitResultCounts = new Map<StellarSubmitResult, number>();
+
 type MetricBucket = {
   route: string;
   method: string;
@@ -133,9 +144,39 @@ export function toPrometheusText() {
     );
   }
 
+  lines.push("# HELP fortexa_decision_outcomes_total Total decision evaluations by outcome");
+  lines.push("# TYPE fortexa_decision_outcomes_total counter");
+  for (const [outcome, count] of decisionOutcomeCounts) {
+    lines.push(`fortexa_decision_outcomes_total{outcome="${outcome}"} ${count}`);
+  }
+
+  lines.push("# HELP fortexa_stellar_submit_results_total Total Stellar submission attempts by result");
+  lines.push("# TYPE fortexa_stellar_submit_results_total counter");
+  for (const [result, count] of stellarSubmitResultCounts) {
+    lines.push(`fortexa_stellar_submit_results_total{result="${result}"} ${count}`);
+  }
+
   return `${lines.join("\n")}\n`;
+}
+
+export function recordDecisionOutcome(outcome: DecisionOutcome) {
+  decisionOutcomeCounts.set(outcome, (decisionOutcomeCounts.get(outcome) ?? 0) + 1);
+}
+
+export function recordStellarSubmitResult(result: StellarSubmitResult) {
+  stellarSubmitResultCounts.set(result, (stellarSubmitResultCounts.get(result) ?? 0) + 1);
 }
 
 export function resetMetrics() {
   buckets.clear();
+  decisionOutcomeCounts.clear();
+  stellarSubmitResultCounts.clear();
+}
+
+export function getDecisionOutcomeCounts(): ReadonlyMap<DecisionOutcome, number> {
+  return decisionOutcomeCounts;
+}
+
+export function getStellarSubmitResultCounts(): ReadonlyMap<StellarSubmitResult, number> {
+  return stellarSubmitResultCounts;
 }

Nexusopen-source/nexusguard

src/lib/observability/metrics.test.ts
@@ -1,6 +1,15 @@
 import { beforeEach, describe, expect, it } from "vitest";
 
-import { getMetricsSnapshot, recordApiMetric, resetMetrics, toPrometheusText } from "@/lib/observability/metrics";
+import {
+  getDecisionOutcomeCounts,
+  getMetricsSnapshot,
+  getStellarSubmitResultCounts,
+  recordApiMetric,
+  recordDecisionOutcome,
+  recordStellarSubmitResult,
+  resetMetrics,
+  toPrometheusText,
+} from "@/lib/observability/metrics";
 
 describe("observability metrics", () => {
   beforeEach(() => {
@@ -28,5 +37,66 @@ describe("observability metrics", () => {
 
     expect(output).toContain("nexusguard_requests_total");
     expect(output).toContain('route="/api/policy"');
+  });
+
+  it("increments decision outcome counters (success path)", () => {
+    recordDecisionOutcome("APPROVE");
+    recordDecisionOutcome("APPROVE");
+    recordDecisionOutcome("WARN");
+
+    const counts = getDecisionOutcomeCounts();
+    expect(counts.get("APPROVE")).toBe(2);
+    expect(counts.get("WARN")).toBe(1);
+    expect(counts.get("REQUIRE_APPROVAL")).toBeUndefined();
+    expect(counts.get("BLOCK")).toBeUndefined();
+  });
+
+  it("increments decision outcome counters (failure path)", () => {
+    recordDecisionOutcome("BLOCK");
+
+    const counts = getDecisionOutcomeCounts();
+    expect(counts.get("BLOCK")).toBe(1);
+    expect(counts.get("APPROVE")).toBeUndefined();
+  });
+
+  it("increments stellar submit result counters (success path)", () => {
+    recordStellarSubmitResult("success");
+    recordStellarSubmitResult("success");
+    recordStellarSubmitResult("idempotency_replay");
+
+    const counts = getStellarSubmitResultCounts();
+    expect(counts.get("success")).toBe(2);
+    expect(counts.get("idempotency_replay")).toBe(1);
+    expect(counts.get("horizon_failure")).toBeUndefined();
+  });
+
+  it("increments stellar submit result counters (failure path)", () => {
+    recordStellarSubmitResult("horizon_failure");
+    recordStellarSubmitResult("idempotency_conflict");
+
+    const counts = getStellarSubmitResultCounts();
+    expect(counts.get("horizon_failure")).toBe(1);
+    expect(counts.get("idempotency_conflict")).toBe(1);
+    expect(counts.get("success")).toBeUndefined();
+  });
+
+  it("includes new counters in prometheus text output", () => {
+    recordDecisionOutcome("APPROVE");
+    recordStellarSubmitResult("success");
+
+    const output = toPrometheusText();
+    expect(output).toContain("nexusguard_decision_outcomes_total");
+    expect(output).toContain('outcome="APPROVE"');
+    expect(output).toContain("nexusguard_stellar_submit_results_total");
+    expect(output).toContain('result="success"');
+  });
+
+  it("resets new counters alongside existing buckets", () => {
+    recordDecisionOutcome("WARN");
+    recordStellarSubmitResult("success");
+    resetMetrics();
+
+    expect(getDecisionOutcomeCounts().size).toBe(0);
+    expect(getStellarSubmitResultCounts().size).toBe(0);
   });
 });

src/lib/observability/metrics.ts
@@ -1,5 +1,16 @@
 type MetricKey = `${string}:${string}`;
 
+export type DecisionOutcome = "APPROVE" | "WARN" | "REQUIRE_APPROVAL" | "BLOCK";
+export type StellarSubmitResult =
+  | "success"
+  | "horizon_failure"
+  | "validation_failure"
+  | "idempotency_replay"
+  | "idempotency_conflict";
+
+const decisionOutcomeCounts = new Map<DecisionOutcome, number>();
+const stellarSubmitResultCounts = new Map<StellarSubmitResult, number>();
+
 type MetricBucket = {
   route: string;
   method: string;
@@ -133,9 +144,39 @@ export function toPrometheusText() {
     );
   }
 
+  lines.push("# HELP nexusguard_decision_outcomes_total Total decision evaluations by outcome");
+  lines.push("# TYPE nexusguard_decision_outcomes_total counter");
+  for (const [outcome, count] of decisionOutcomeCounts) {
+    lines.push(`nexusguard_decision_outcomes_total{outcome="${outcome}"} ${count}`);
+  }
+
+  lines.push("# HELP nexusguard_stellar_submit_results_total Total Stellar submission attempts by result");
+  lines.push("# TYPE nexusguard_stellar_submit_results_total counter");
+  for (const [result, count] of stellarSubmitResultCounts) {
+    lines.push(`nexusguard_stellar_submit_results_total{result="${result}"} ${count}`);
+  }
+
   return `${lines.join("\n")}\n`;
+}
+
+export function recordDecisionOutcome(outcome: DecisionOutcome) {
+  decisionOutcomeCounts.set(outcome, (decisionOutcomeCounts.get(outcome) ?? 0) + 1);
+}
+
+export function recordStellarSubmitResult(result: StellarSubmitResult) {
+  stellarSubmitResultCounts.set(result, (stellarSubmitResultCounts.get(result) ?? 0) + 1);
 }
 
 export function resetMetrics() {
   buckets.clear();
+  decisionOutcomeCounts.clear();
+  stellarSubmitResultCounts.clear();
+}
+
+export function getDecisionOutcomeCounts(): ReadonlyMap<DecisionOutcome, number> {
+  return decisionOutcomeCounts;
+}
+
+export function getStellarSubmitResultCounts(): ReadonlyMap<StellarSubmitResult, number> {
+  return stellarSubmitResultCounts;
 }